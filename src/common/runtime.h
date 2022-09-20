788ba09324f60b79eb2e53ef7594ac0c524243d3
LoveBettygirl <bettycyygirl@163.com>
2022-09-20 14:21:03 +0800

    Add runtime

---
+++ src/common/runtime.h
@@ -0,0 +1,17 @@
+#ifndef RUNTIME_H
+#define RUNTIME_H
+
+#include <string>
+
+namespace corpc {
+
+class RunTime {
+public:
+    std::string msgNo_;
+    std::string interfaceName_;
+};
+
+}
+
+
+#endif
