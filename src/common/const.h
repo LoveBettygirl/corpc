4cfc9935eda0f0b0368caf9b2c2e7ac347d04a47
LoveBettygirl <bettycyygirl@163.com>
2022-09-23 15:41:32 +0800

    Add const.h

---
+++ src/common/const.h
@@ -0,0 +1,22 @@
+#ifndef CONST_H
+#define CONST_H
+
+namespace corpc {
+
+enum LogType {
+    INTER_LOG = 1, // 框架内部日志
+    USER_LOG = 2, // 用户日志
+};
+
+enum LogLevel {
+    NONE = 0, // don't print log
+    DEBUG = 1,
+    INFO = 2,
+    WARN = 3,
+    ERROR = 4,
+    FATAL = 5
+};
+
+}
+
+#endif
