a9bee39e6f9c14099c821f11d94924a87a40da51
LoveBettygirl <bettycyygirl@163.com>
2022-10-07 22:09:12 +0800

    Add timewheel

---
+++ src/net/tcp/timewheel.h
@@ -0,0 +1,36 @@
+#ifndef TIMEWHEEL_H
+#define TIMEWHEEL_H
+
+#include <queue>
+#include <vector>
+#include "abstractslot.h"
+#include "eventloop.h"
+#include "timer.h"
+
+namespace corpc {
+
+class TcpConnection;
+
+class TcpTimeWheel {
+
+public:
+    typedef std::shared_ptr<TcpTimeWheel> ptr;
+    typedef AbstractSlot<TcpConnection> TcpConnectionSlot;
+    TcpTimeWheel(EventLoop *loop, int bucketCount, int invertal = 10);
+    ~TcpTimeWheel();
+
+    void fresh(TcpConnectionSlot::ptr slot);
+    void loopFunc();
+
+private:
+    EventLoop *loop_{nullptr};
+    int bucketCount_{0};
+    int interval_{0}; // second
+
+    TimerEvent::ptr event_;
+    std::queue<std::vector<TcpConnectionSlot::ptr>> wheel_;
+};
+
+}
+
+#endif
