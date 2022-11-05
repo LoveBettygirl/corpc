1461dd7d34fd512047f8a7e3ff548bf007a4b2bc
LoveBettygirl <bettycyygirl@163.com>
2022-11-05 14:03:29 +0800

    Add http_request.h

---
+++ src/net/http/http_request.h
@@ -0,0 +1,29 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+#include <string>
+#include <memory>
+#include <map>
+
+#include "abstract_data.h"
+#include "http_define.h"
+
+namespace corpc {
+
+class HttpRequest : public AbstractData {
+public:
+    typedef std::shared_ptr<HttpRequest> ptr;
+
+public:
+    HttpMethod requestMethod_;
+    std::string requestPath_;
+    std::string requestQuery_;
+    std::string requestVersion_;
+    std::string requestBody_;
+
+    std::map<std::string, std::string> queryMaps_;
+};
+
+}
+
+#endif
