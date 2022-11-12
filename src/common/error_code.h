2ecc986f87d1478e015777e5b6749d39020881b6
LoveBettygirl <bettycyygirl@163.com>
2022-11-12 19:12:33 +0800

    Add pb_rpc_controller

--- src/common/error_code.h
+++ src/common/error_code.h
@@ -23,7 +23,7 @@ const int ERROR_SERVICE_NOT_FOUND = SYS_ERROR_PREFIX(0008); // not found service
 
 const int ERROR_METHOD_NOT_FOUND = SYS_ERROR_PREFIX(0009); // not found method
 
-const int ERROR_PARSE_SERVICE_NAME = SYS_ERROR_PREFIX(0010);     // not found service name
+const int ERROR_PARSE_SERVICE_FULL_NAME = SYS_ERROR_PREFIX(0010);     // not found service name
 const int ERROR_NOT_SET_ASYNC_PRE_CALL = SYS_ERROR_PREFIX(0011); // you didn't set some nessary param before call async rpc
 const int ERROR_CONNECT_SYS_ERR = SYS_ERROR_PREFIX(0012);        // connect sys error
 
---
+++ src/net/pb/pb_rpc_controller.h
@@ -0,0 +1,70 @@
+#ifndef PB_RPC_CONRTOLLER_H
+#define PB_RPC_CONRTOLLER_H
+
+#include <google/protobuf/service.h>
+#include <google/protobuf/stubs/callback.h>
+#include <stdio.h>
+#include <memory>
+#include "netaddress.h"
+
+namespace corpc {
+
+class PbRpcController : public google::protobuf::RpcController {
+
+public:
+    typedef std::shared_ptr<PbRpcController> ptr;
+    // Client-side methods ---------------------------------------------
+
+    PbRpcController() = default;
+    ~PbRpcController() = default;
+
+    void Reset() override;
+    bool Failed() const override;
+
+    // Server-side methods ---------------------------------------------
+
+    std::string ErrorText() const override;
+    void StartCancel() override;
+    void SetFailed(const std::string &reason) override;
+    bool IsCanceled() const override;
+    void NotifyOnCancel(google::protobuf::Closure *callback) override;
+
+    // common methods
+
+    int errorCode() const;
+    void setErrorCode(const int errorCode);
+    const std::string &msgSeq() const;
+    void setMsgReq(const std::string &msgReq);
+    void setError(const int errCode, const std::string &errInfo);
+    void setPeerAddr(NetAddress::ptr addr);
+    void setLocalAddr(NetAddress::ptr addr);
+
+    NetAddress::ptr peerAddr();
+    NetAddress::ptr localAddr();
+
+    void setTimeout(const int timeout);
+    int timeout() const;
+
+    void setMethodName(const std::string &name);
+    std::string getMethodName();
+
+    void setMethodFullName(const std::string &name);
+    std::string getMethodFullName();
+
+private:
+    int errorCode_{0};      // errorCode, identify one specific error
+    std::string errorInfo_; // errorInfo, details description of error
+    std::string msgReq_;    // msgReq, identify once rpc request and response
+    bool isFailed_{false};
+    bool isCanceled{false};
+    NetAddress::ptr peerAddr_;
+    NetAddress::ptr localAddr_;
+
+    int timeout_{5000};       // max call rpc timeout
+    std::string methodName_; // method name
+    std::string fullName_;   // full name, like server.method_name
+};
+
+}
+
+#endif
